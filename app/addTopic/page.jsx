8dfc5fde6accf90bb27d362379f30ab77e060110
abdulmoid648 <abdulmoid648@gmail.com>
2025-11-07 14:44:38 +0000

    Change API endpoint to relative path
    page.jsx

--- app/addTopic/page.jsx
+++ app/addTopic/page.jsx
@@ -15,7 +15,7 @@ export default function AddTopic(){
        }
 
        try {
-        const res = await fetch("http://localhost:3000/api/topics", {
+        const res = await fetch("/api/topics", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
